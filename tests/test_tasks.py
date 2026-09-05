8b558353da3570c116edc3e1a24651359c6fdb5c
SAMRIDHI <saxenasamridhi1921@gmail.com>
2026-09-05 14:24:00 +0530

    Moodified file

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,31 +1,35 @@
 from fastapi.testclient import TestClient
+
 from app.main import app
 
-client=TestClient(app)
+
+client = TestClient(app)
+
 
 def test_root():
-    response=client.get("/")
+    response = client.get("/")
 
-    assert response.status_code==200
-    assert response.json()=={
-        "message":"devops platform is running !"
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "DevOps Task Platform is running!"
     }
 
 
 def test_health_check():
-    response=client.get("/health")
+    response = client.get("/health")
 
-    assert response.status_code==200
-    assert response.json()=={
-        "status":"healthy"
+    assert response.status_code == 200
+    assert response.json() == {
+        "status": "healthy"
     }
 
+
 def test_get_tasks():
-    response=client.get("/tasks")
+    response = client.get("/tasks")
 
-    assert response.status_code==200
+    assert response.status_code == 200
 
-    data=response.json()
+    data = response.json()
+
     assert "tasks" in data
-    assert len(data["tasks"])==2
-
+    assert len(data["tasks"]) == 2
